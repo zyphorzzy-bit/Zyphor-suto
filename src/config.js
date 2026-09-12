2a169e31ce170aaeff04a2b41f92043217739052
zyphorzzy-bit <zyphorzzy@gmail.com>
2026-09-12 04:51:56 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,10 @@
-
+module.exports = {
+  botName: "Zyphor Zombie",
+  version: "2.0.0",
+  prefix: ".",
+  ownerNumber: "5512988625367@s.whatsapp.net",
+  officialLinks: {
+    channel: "https://whatsapp.com/channel/0029Vb98ckOJJhzeY2G8sI2R",
+    group: "https://chat.whatsapp.com/KKndUto6ZyYL0EVQ2rkB1r"
+  }
+};
